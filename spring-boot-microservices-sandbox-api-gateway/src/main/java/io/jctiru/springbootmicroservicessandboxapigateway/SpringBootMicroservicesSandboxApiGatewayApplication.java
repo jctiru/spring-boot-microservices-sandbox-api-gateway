ebf2cc0e7bba05a6f5861370c719a6ca0f37fdaa
jctiru <jonathanchristiantiru@gmail.com>
2020-05-17 20:52:15 +0800

    Initial commit

---
+++ spring-boot-microservices-sandbox-api-gateway/src/main/java/io/jctiru/springbootmicroservicessandboxapigateway/SpringBootMicroservicesSandboxApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package io.jctiru.springbootmicroservicessandboxapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringBootMicroservicesSandboxApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservicesSandboxApiGatewayApplication.class, args);
+	}
+
+}
---
+++ spring-boot-microservices-sandbox-api-gateway/src/test/java/io/jctiru/springbootmicroservicessandboxapigateway/SpringBootMicroservicesSandboxApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jctiru.springbootmicroservicessandboxapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesSandboxApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
