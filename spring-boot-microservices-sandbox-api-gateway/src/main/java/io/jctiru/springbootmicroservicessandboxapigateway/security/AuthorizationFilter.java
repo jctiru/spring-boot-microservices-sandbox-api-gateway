b0d7cb063c40509c6674322f47cc4cbf9af715d0
jctiru <jonathanchristiantiru@gmail.com>
2020-05-24 18:13:20 +0800

    Create AuthorizationFilter

---
+++ spring-boot-microservices-sandbox-api-gateway/src/main/java/io/jctiru/springbootmicroservicessandboxapigateway/security/AuthorizationFilter.java
@@ -0,0 +1,74 @@
+package io.jctiru.springbootmicroservicessandboxapigateway.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+
+@Component
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+	@Autowired
+	private Environment env;
+
+	@Autowired
+	public AuthorizationFilter(@Lazy AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		String header = req.getHeader(env.getProperty("authorization.token.header.name"));
+
+		if (header == null || !header.startsWith(env.getProperty("authorization.token.header.prefix"))) {
+			chain.doFilter(req, res);
+			System.out.println("Header not found");
+			return;
+		}
+
+		System.out.println("Header found: " + header);
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		chain.doFilter(req, res);
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+		String token = request.getHeader(env.getProperty("authorization.token.header.name"));
+
+		System.out.println(token);
+		
+		if (token != null) {
+			token = token.replace(env.getProperty("authorization.token.header.prefix"), "");
+
+			Jws<Claims> parsedToken = Jwts.parser()
+					.setSigningKey(env.getProperty("token.secret"))
+					.parseClaimsJws(token);
+
+			String userId = parsedToken.getBody().getSubject();
+
+			if (userId != null) {
+				return new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+			}
+		}
+
+		return null;
+	}
+
+}
