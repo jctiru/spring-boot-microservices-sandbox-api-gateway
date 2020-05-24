f12338bd29233202917e3d08dc31100d18ad16de
jctiru <jonathanchristiantiru@gmail.com>
2020-05-24 18:13:49 +0800

    Use AuthorizationFilter in WebSecurity

--- spring-boot-microservices-sandbox-api-gateway/src/main/java/io/jctiru/springbootmicroservicessandboxapigateway/security/WebSecurity.java
+++ spring-boot-microservices-sandbox-api-gateway/src/main/java/io/jctiru/springbootmicroservicessandboxapigateway/security/WebSecurity.java
@@ -1,8 +1,10 @@
 package io.jctiru.springbootmicroservicessandboxapigateway.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,21 +14,30 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Autowired
+	private AuthorizationFilter authorizationFilter;
+
+	@Autowired
 	private Environment env;
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 				.authorizeRequests()
-				.antMatchers(HttpMethod.GET, env.getProperty("api.statuscheck.url.path")).permitAll()
 				.antMatchers(env.getProperty("api.h2console.url.path")).permitAll()
 				.antMatchers(HttpMethod.POST, env.getProperty("api.registration.url.path")).permitAll()
 				.antMatchers(HttpMethod.POST, env.getProperty("api.login.url.path")).permitAll()
 				.anyRequest().authenticated()
 				.and()
+				.addFilter(authorizationFilter)
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
 				.headers().frameOptions().disable();
+	}
+
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManager();
 	}
 
 }
